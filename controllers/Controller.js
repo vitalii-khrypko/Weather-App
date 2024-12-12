461207daa7dc37b9a50e1a960d0814c8761c8f7e
Vitalii Khrypko <00hripkovitaliy00@gmail.com>
2024-12-12 19:47:15 +0200

    ref

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -3,76 +3,18 @@ export class Controller {
         this.model = model;
         this.view = view;
 
-        // // Завантаження завдань з localStorage
-        // this.model.loadTasks();
-        //
-        // // Встановлення обробників подій
-        // this.view.addTaskBtn.addEventListener("click", () => this.addTask());
-        // this.view.listContainer.addEventListener("click", e => this.handleListClick(e));
-        // document.body.addEventListener("keydown", e => this.handleEnterKey(e));
-        //
-        // // Початковий рендер
-        // this.view.renderTasks(this.model.getTasks());
+        this.view.showBtn.addEventListener("click", () => this.showData());
+
     }
 
-    // addTask() {
-    //     const taskText = this.view.inputBox.value.trim();
-    //     if (taskText) {
-    //         this.model.addTask(taskText);
-    //         this.model.saveTasks(); // Збереження до localStorage
-    //         this.view.renderTasks(this.model.getTasks());
-    //         this.view.clearInput();
-    //     }
-    // }
-    //
-    // handleListClick(event) {
-    //     const target = event.target;
-    //
-    //     if (target.classList.contains("edit")) {
-    //         this.editTask(target);
-    //     } else if (target.tagName === "LI") {
-    //         target.classList.toggle("checked");
-    //     } else if (target.tagName === "SPAN") {
-    //         this.deleteTask(target);
-    //     }
-    // }
-    //
-    // editTask(editButton) {
-    //     const li = editButton.parentElement;
-    //     const inputField = document.createElement("input");
-    //     inputField.type = "text";
-    //     inputField.value = li.firstChild.textContent.trim();
-    //
-    //     const index = Array.from(this.view.listContainer.children).indexOf(li);
-    //
-    //     li.firstChild.replaceWith(inputField);
-    //     inputField.focus();
-    //
-    //     inputField.addEventListener("blur", () => {
-    //         const updatedTask = inputField.value.trim();
-    //         this.model.editTask(index, updatedTask);
-    //         this.model.saveTasks(); // Збереження до localStorage
-    //         this.view.renderTasks(this.model.getTasks());
-    //     });
-    //
-    //     inputField.addEventListener("keydown", e => {
-    //         if (e.key === "Enter") {
-    //             inputField.blur();
-    //         }
-    //     });
-    // }
-    //
-    // deleteTask(deleteButton) {
-    //     const li = deleteButton.parentElement;
-    //     const index = Array.from(this.view.listContainer.children).indexOf(li);
-    //     this.model.removeTask(index);
-    //     this.model.saveTasks(); // Збереження до localStorage
-    //     this.view.renderTasks(this.model.getTasks());
-    // }
-    //
-    // handleEnterKey(event) {
-    //     if (event.key === "Enter" && this.view.inputBox.value !== "") {
-    //         this.addTask();
-    //     }
-    // }
+    getData(weather) {
+        this.view.showData((weather) => {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.view.input.value}&appid=3d3ebf6246c40a1fef67004a8905fa30`;
+            fetch(url)
+                .then(response => response.json())
+                .then(json => {
+
+                });
+        });
+    }
 }
--- models/Model.js
+++ models/Model.js
@@ -11,33 +11,4 @@ export class Model {
     get temp() {
         return this.temp = weather.temp;
     }
-
-    // loadTasks() {
-    //     const tasksFromStorage = localStorage.getItem("tasks");
-    //     this.tasks = tasksFromStorage ? JSON.parse(tasksFromStorage) : [];
-    // }
-    //
-    // saveTasks() {
-    //     localStorage.setItem("tasks", JSON.stringify(this.tasks));
-    // }
-    //
-    // getTasks() {
-    //     return this.tasks;
-    // }
-    //
-    // addTask(task) {
-    //     this.tasks.push(task);
-    // }
-    //
-    // editTask(index, newTask) {
-    //     if (index >= 0 && index < this.tasks.length) {
-    //         this.tasks[index] = newTask;
-    //     }
-    // }
-    //
-    // removeTask(index) {
-    //     if (index >= 0 && index < this.tasks.length) {
-    //         this.tasks.splice(index, 1);
-    //     }
-    // }
 }
