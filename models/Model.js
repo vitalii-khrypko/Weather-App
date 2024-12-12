803e4e65006f188c5660aaa20e6cd602757a4694
Vitalii Khrypko <00hripkovitaliy00@gmail.com>
2024-12-12 18:45:37 +0200

    first commit

---
+++ models/Model.js
@@ -0,0 +1,34 @@
+export class Model {
+    constructor() {
+        //this.tasks = [];
+    }
+
+    // loadTasks() {
+    //     const tasksFromStorage = localStorage.getItem("tasks");
+    //     this.tasks = tasksFromStorage ? JSON.parse(tasksFromStorage) : [];
+    // }
+    //
+    // saveTasks() {
+    //     localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    // }
+    //
+    // getTasks() {
+    //     return this.tasks;
+    // }
+    //
+    // addTask(task) {
+    //     this.tasks.push(task);
+    // }
+    //
+    // editTask(index, newTask) {
+    //     if (index >= 0 && index < this.tasks.length) {
+    //         this.tasks[index] = newTask;
+    //     }
+    // }
+    //
+    // removeTask(index) {
+    //     if (index >= 0 && index < this.tasks.length) {
+    //         this.tasks.splice(index, 1);
+    //     }
+    // }
+}
